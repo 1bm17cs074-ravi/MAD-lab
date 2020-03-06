cac5c66892c5d7a83f222fa7554c03da350efde3
1bm17cs074-ravi <53852194+1bm17cs074-ravi@users.noreply.github.com>
2020-03-06 10:43:05 +0530

    Add files via upload

---
+++ SecondActivity.java
@@ -0,0 +1,44 @@
+package com.example.myapplication;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText t1 = (EditText)findViewById(R.id.name);
+        final EditText t2 = (EditText)findViewById(R.id.usn);
+        final EditText t3 = (EditText)findViewById(R.id.age);
+
+        Button b = (Button)findViewById(R.id.display);
+
+        b.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+
+                String s1 = t1.getText().toString();
+                String s2 = t2.getText().toString();
+                String s3 = t3.getText().toString();
+
+                intent.putExtra("Name",s1);
+                intent.putExtra("USN",s2);
+                intent.putExtra("Branch",s3);
+
+                startActivity(intent);
+
+            }
+        });
+
+    }
+}
